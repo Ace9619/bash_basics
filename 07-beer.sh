c86e23876c7d738305f10aadd040d499d078445e
Austin Edwards <Aedwards12@ggc.edu>
2019-09-12 18:39:40 -0400

    added 2nd song

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,19 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+
+while [ $count -ge 0 ]; do
+  if [ $count -ge 2 ]; then
+      echo "$count cans of tuna on the wall, $count cans of tuna"
+      echo "Take one down pass it around"
+  elif [ $count -eq 1 ]; then
+      echo "$count cans of tuna on the wall, $count cans of tuna"
+      echo "Take one down pass it around"
+  else
+      echo "no more cans of tuna on the wall"
+  fi
+  
+  # the following statement is equivalent to: let "count=count-1"
+  ((count = count - 1))
+
+done
